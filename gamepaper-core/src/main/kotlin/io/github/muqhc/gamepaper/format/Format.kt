8702dce58306943b1cdd8aaf4d70e4a3ef6eb7d8
muqhc <muqhc07@gmail.com>
2022-04-26 17:32:48 +0900

    fix format method

--- gamepaper-core/src/main/kotlin/io/github/muqhc/gamepaper/format/Format.kt
+++ gamepaper-core/src/main/kotlin/io/github/muqhc/gamepaper/format/Format.kt
@@ -1,6 +1,6 @@
 package io.github.muqhc.gamepaper.format
 
 interface Format<TargetType,ResourceType> {
-    fun checkIsValid(target: ResourceType): Boolean
+    fun checkIsValid(target: ResourceType)
     fun construct(target: ResourceType): TargetType
 }
