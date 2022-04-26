4db9c3dc493ee725f90a5ea33aafb3009e0cba35
muqhc <muqhc07@gmail.com>
2022-04-26 17:26:28 +0900

    improve exceptions

--- gamepaper-core/src/main/kotlin/io/github/muqhc/gamepaper/exception/FormatterNotFoundException.kt
+++ gamepaper-core/src/main/kotlin/io/github/muqhc/gamepaper/exception/FormatterNotFoundException.kt
@@ -1,5 +1,7 @@
 package io.github.muqhc.gamepaper.exception
 
-class FormatterNotFoundException(message: String): Exception(message) {
-    constructor(): this("")
+class FormatterNotFoundException: Exception {
+    constructor(): super()
+    constructor(message: String): super(message)
+    constructor(message: String,cause: Throwable): super(message,cause)
 }
--- gamepaper-core/src/main/kotlin/io/github/muqhc/gamepaper/exception/InvalidConfigException.kt
+++ gamepaper-core/src/main/kotlin/io/github/muqhc/gamepaper/exception/InvalidConfigException.kt
@@ -1,3 +1,7 @@
 package io.github.muqhc.gamepaper.exception
 
-class InvalidConfigException(message: String): Exception(message)
+class InvalidConfigException: Exception {
+    constructor(): super()
+    constructor(message: String): super(message)
+    constructor(message: String,cause: Throwable): super(message,cause)
+}
