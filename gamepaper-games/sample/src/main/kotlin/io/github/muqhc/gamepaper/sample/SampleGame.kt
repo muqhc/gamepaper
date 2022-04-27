81f7e2344830e75d1c119637bff0c2ee5e4132d7
muqhc <muqhc07@gmail.com>
2022-04-27 23:16:01 +0900

    message onEnable

--- gamepaper-games/sample/src/main/kotlin/io/github/muqhc/gamepaper/sample/SampleGame.kt
+++ gamepaper-games/sample/src/main/kotlin/io/github/muqhc/gamepaper/sample/SampleGame.kt
@@ -3,5 +3,7 @@ package io.github.muqhc.gamepaper.sample
 import io.github.muqhc.gamepaper.Game
 
 class SampleGame: Game<SampleGameConfig>() {
-
+    override fun onEnable() {
+        println("Hello Gamepaper!")
+    }
 }
