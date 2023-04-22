95a6122ec367f8e41e859e83e8a9f1763a227e3b
muqhc <muqhc07@gmail.com>
2023-04-22 11:53:27 +0900

    up-to-date dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "1.6.10"
+    kotlin("jvm") version "1.8.10"
 }
 
 allprojects {
@@ -15,8 +15,11 @@ subprojects {
         implementation(kotlin("stdlib"))
         implementation(kotlin("reflect"))
         implementation("io.github.muqhc:skolloble-parser:1.5.1")
+        if (name.contains("bukkit")) {
+            compileOnly("io.papermc.paper:paper-api:1.18.2-R0.1-SNAPSHOT")
+            testImplementation("io.papermc.paper:paper-api:1.18.2-R0.1-SNAPSHOT")
+        }
         if (!name.endsWith("core")) {
-            implementation("io.papermc.paper:paper-api:1.18.2-R0.1-SNAPSHOT")
             implementation(project(":${rootProject.name}-core"))
         }
     }
